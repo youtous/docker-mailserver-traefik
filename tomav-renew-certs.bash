7b56bf59076751e13e9aaf8c54c4e15ba1d40f3b
youtous <contact@youtous.me>
2020-04-10 17:29:48 +0200

    update tomav renew script

--- tomav-renew-certs.bash
+++ tomav-renew-certs.bash
@@ -14,39 +14,52 @@ Newkey=/tmp/ssl/privkey.pem
 Backupkey="$Newkey.backup"
 Backupcert="$Newcert.backup"
 
-#check if a new cert is present
-echo "newcert is $Newcert"
-if [ -f "$Newcert" ]
-then
-  echo "new certificate detected"
-  #take fingerprints
-  FP_CurrentC=`openssl x509 -fingerprint -nocert -in $Currentcert`
-  FP_NewC=`openssl x509 -fingerprint -nocert -in $Newcert`
-  echo "FP Curry is $FP_CurrentC"
-  echo "FP New is $FP_NewC"
-  #check sanity pemfile
-  stringlenght=${#FP_NewC}  #fingerprint should be a long string
-  echo " length is $stringlenght"
-  if [ $stringlenght -gt 50 ]
-  then
-          echo sane
-          #test if FP are different, if yes let take actions
-          if [ "$FP_NewC" = "$FP_CurrentC" ]
-          then
-                  echo " FPs match, do nothing"
-                  logger "Cert update: certificate update started, but something went wrong, check newcerts perhaps"
-          else
-                  cp $Newcert $Currentcert
-                  cp $Newkey $Currentkey
-                  chmod 600 $Currentcert
-                  chmod 600 $Currentkey
-                  logger "Cert update: new certificates have been copied into container"
-                  logger "Cert update: restarting daemons Postfix and Dovecot"
-                  supervisorctl restart postfix
-                  supervisorctl restart dovecot
-                mv $Newkey $Backupkey
-                  mv $Newcert $Backupcert
-          fi
-  fi
-logger "Cert update: Newcert script exited fine"
+if [ ! -f "$Newcert" ]; then
+    echo "[ERROR] renew certificates script called without submitting a new certificate in $Newcert"
+    exit 1
+fi
+if [ ! -f "Newkey" ]; then
+    echo "[ERROR] renew certificates script called without submitting a new key in $Newkey"
+    exit 1
+fi
+
+
+echo "[INFO] new certificate '$Newcert' received on mailserver container"
+
+# take fingerprints
+FP_CurrentC=`openssl x509 -fingerprint -nocert -in $Currentcert`
+FP_NewC=`openssl x509 -fingerprint -nocert -in $Newcert`
+
+#fingerprint should be a long string
+stringlenght=${#FP_NewC}
+if [ ! $stringlenght -gt 50 ]; then
+    echo "[ERROR] fingerprint of new certificate is too short = $stringlenght, dealing with an invalid certificate"
+    exit 1
+fi
+
+
+# echo "[DEBUG] FP Currentcert is $FP_CurrentC"
+# echo "[DEBUG] FP Newcert is $FP_NewC"
+
+
+#test if FP are different, if yes let take actions
+if [ "$FP_NewC" = "$FP_CurrentC" ]; then
+  echo "[DEBUG] FPs match, do nothing"
+else
+
+  # cp new certificate, update permissions
+  cp $Newcert $Currentcert
+  cp $Newkey $Currentkey
+  chmod 600 $Currentcert
+  chmod 600 $Currentkey
+
+  logger "[INFO] Cert update: new certificates have been copied into container"
+
+  logger "[INFO] Cert update: restarting daemons Postfix and Dovecot"
+  supervisorctl restart postfix
+  supervisorctl restart dovecot
+
+  # move to backup for memory
+  mv $Newkey $Backupkey
+  mv $Newcert $Backupcert
 fi
